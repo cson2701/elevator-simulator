3d722778e4d194a7a376a48d62df5a7b4ce402af
gavin song <gavin.song@gumtreegroup.com.au>
2024-03-21 21:28:12 +1100

    Open ConfigActivity if hi/lo floors not set

--- app/src/main/java/com/example/elevatorsimulator/MainActivity.kt
+++ app/src/main/java/com/example/elevatorsimulator/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.elevatorsimulator
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,6 +10,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.elevatorsimulator.elevator.ElevatorViewModel
+import com.example.elevatorsimulator.elevator.config.ConfigActivity
+import com.example.elevatorsimulator.elevator.config.ElevatorConfig
 import com.example.elevatorsimulator.elevator.views.ElevatorView
 import com.example.elevatorsimulator.ui.theme.ElevatorSimulatorTheme
 
@@ -26,6 +29,14 @@ class MainActivity : ComponentActivity() {
                     ElevatorView(elevatorViewModel)
                 }
             }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (ElevatorConfig().getLowestFloor() == 0 || ElevatorConfig().getHighestFloor() == 0) {
+            startActivity(Intent(this, ConfigActivity::class.java))
+            finish()
         }
     }
 }
