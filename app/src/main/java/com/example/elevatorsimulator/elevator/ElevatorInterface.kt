c50726da5b3d485fc33cc9f9af07b8e03f1cbd51
song_cr <gavinsong727@gmail.com>
2024-04-02 10:58:30 +1100

    Implement target floor reach as a callback

--- app/src/main/java/com/example/elevatorsimulator/elevator/ElevatorInterface.kt
+++ app/src/main/java/com/example/elevatorsimulator/elevator/ElevatorInterface.kt
@@ -1,7 +1,10 @@
 package com.example.elevatorsimulator.elevator
 
 interface ElevatorInterface {
-    suspend fun move(targetFloor: Int): Boolean
+    suspend fun move(
+        targetFloor: Int,
+        onTargetFloorReached: (isTargetFloorReached: Boolean) -> Unit,
+    ): Boolean
 
     fun status(): ElevatorProps.Status
 
