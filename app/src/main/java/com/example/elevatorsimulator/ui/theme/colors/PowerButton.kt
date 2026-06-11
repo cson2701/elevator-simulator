365e6ae968ac6502e8a21a605e8b3d7f19b54c55
Gavin Song <gavin.song@gumtreegroup.com.au>
2026-06-11 15:06:49 +1000

    [65] Flash power button when powering on

---
+++ app/src/main/java/com/example/elevatorsimulator/ui/theme/colors/PowerButton.kt
@@ -0,0 +1,31 @@
+package com.example.elevatorsimulator.ui.theme.colors
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import com.example.elevatorsimulator.R
+
+object PowerButton {
+    object On
+    object Off
+}
+
+val PowerButton.On.Default: Color
+    @Composable
+    get() = colorResource(id = R.color.teal_200)
+
+val PowerButton.On.Pressed: Color
+    @Composable
+    get() = colorResource(id = R.color.teal_700)
+
+val PowerButton.Off.Default: Color
+    @Composable
+    get() = colorResource(id = R.color.grey)
+
+val PowerButton.Off.Pressed: Color
+    @Composable
+    get() = colorResource(id = R.color.dark_grey)
+
+val PowerButton.Icon: Color
+    @Composable
+    get() = Color.White
